dda3c7c09a46b36f14d82eebeda157594b9c76de
Dawson Toth <dawsont@ifit.com>
2024-09-23 23:35:04 -0400

    fix: Immediately write lines

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Atem } = require('atem-connection');
-const rpio = require('rpio');
+const { open, write, LOW, HIGH, OUTPUT } = require('rpio');
 
 process.on('SIGINT', cleanUp);
 
@@ -14,25 +14,25 @@ const atemIP = '192.168.1.47';
 const lines = [
   // physical pins start at the top left, pins toward you, with the USB ports pointed downward, on the 3b
   {
-    // main
+    // Main
     input: 1,
     preview: 15,
     program: 16,
   },
-  // tripod
   {
+    // Tripod
     input: 9,
     preview: 31,
     program: 32,
   },
-  // flycam
   {
+    // Flycam
     input: 10,
     preview: 35,
     program: 36,
   },
-  // handheld
   {
+    // Handheld
     input: 11,
     preview: 37,
     program: 38,
@@ -41,8 +41,8 @@ const lines = [
 
 console.log('Requesting output mode on all linePairs...');
 for (const line of lines) {
-  rpio.open(line.preview, rpio.OUTPUT, rpio.LOW);
-  rpio.open(line.program, rpio.OUTPUT, rpio.LOW);
+  open(line.preview, OUTPUT, LOW);
+  open(line.program, OUTPUT, LOW);
 }
 let previewed = [];
 let programmed = [];
@@ -50,19 +50,15 @@ let programmed = [];
 atem.on('stateChanged', (state, pathToChange) => {
   previewed = state.video.mixEffects.map(me => me.previewInput);
   programmed = state.video.mixEffects.map(me => me.programInput);
-  setTimeout(updateState, 1);
-});
-
-function updateState() {
   for (const line of lines) {
-    rpio.write(line.preview, previewed.includes(line.input) ? rpio.HIGH : rpio.LOW);
-    rpio.write(line.program, programmed.includes(line.input) ? rpio.HIGH : rpio.LOW);
+    write(line.preview, previewed.includes(line.input) ? HIGH : LOW);
+    write(line.program, programmed.includes(line.input) ? HIGH : LOW);
   }
-}
+});
 
 atem.connect(atemIP)
   .then(() => {
-    console.log('Connected. Probably.');
+    console.log('Connected to ATEM.');
   })
   .catch(err => {
     console.error(err);
